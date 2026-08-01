1e05d31f9a1aa23313e65616085889e6de69bbec
OZ <test@test.com>
2026-08-01 13:28:22 +0100

    smaller aac

--- ConvertToAAC.bat
+++ ConvertToAAC.bat
@@ -5,7 +5,5 @@ rem winget install ffmpeg
 
 for /f "tokens=*" %%a in ('dir /b /s *.opus') do (
     echo Converting "%%a" to "%%~dpna.aac"
-    ffmpeg -i "%%a" -c:a aac -q:a 2 "%%~dpna.aac"
+    ffmpeg -i "%%a" -c:a aac -q:a 0.6 -map_metadata 0 -ac 1 "%%~dpna.aac" -y
 )
-
-pause
