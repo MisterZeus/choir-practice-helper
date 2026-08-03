19739ce4b151cf9ef36b7eb6959cc01c8f79028c
OZ <test@test.com>
2026-08-03 13:22:17 +0100

    smaller audio

--- ConvertToAAC.bat
+++ ConvertToAAC.bat
@@ -5,5 +5,5 @@ rem winget install ffmpeg
 
 for /f "tokens=*" %%a in ('dir /b /s *.opus') do (
     echo Converting "%%a" to "%%~dpna.aac"
-    ffmpeg -i "%%a" -c:a aac -q:a 0.6 -map_metadata 0 -ac 1 "%%~dpna.aac" -y
+    ffmpeg -i "%%a" -c:a aac -q:a 0.5 -map_metadata 0 -ac 1 "%%~dpna.aac" -y
 )
--- ConvertToOpusMono.bat
+++ ConvertToOpusMono.bat
@@ -7,13 +7,13 @@ rem winget install ffmpeg
 
 for /r %%F in (*.mp3 *.wav *.flac *.m4a *.aac *.ogg *.wma *.aiff *.alac) do (
     set "input=%%~fF"
-    set "output=%%~dpnF.opus"
+    set "output=%%~dpnF24K.opus"
 
     if /I "%%~xF"==".opus" (
         echo Skipping already-encoded Opus file: "%%~fF"
     ) else (
         echo Converting "!input!" to "!output!"
-        ffmpeg -i "!input!" -c:a libopus -b:a 32k -vbr on -ac 1 -map_metadata 0 -y "!output!"
+        ffmpeg -i "!input!" -c:a libopus -b:a 24k -vbr on -ac 1 -map_metadata 0 -y "!output!"
     )
 )
 
